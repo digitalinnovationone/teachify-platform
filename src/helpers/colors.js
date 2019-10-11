1ca060d32f61d5f9ddfb2cd4a71ab0df2df12a4d
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-10-11 17:10:36 -0300

    feature: Adding a new library to darken hover colors

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,46 +1,43 @@
-/*
-const colors = {
-    background: '#f5f6fa',
-    backgroundAlt: '#fdfdfd',
-    backgroundAltHover: '#f2f2f2',
-    error: '#ff8080',
-    errorHover: '#ff4d4d',
-    errorLight: '#ffe6e6',
-    primary: '#0099ff',
-    primaryHover: '#007acc',
-    primaryLight: '#e6f5ff',
-    success: '#00cc99',
-    successHover: '#009973',
-    text: '#454545',
-    textAlt: '#fdfdfd',
-    textDark: '#333333',
-    textError: '#ff4d4d',
-    textLight: '#b3b3b3',
-    warning: '#ffcc00',
-    warningHover: '#cca300',
-}
+import { darken } from 'polished'
 
-*/
+const background = '#f5f6fa'
+const backgroundAlt = '#fdfdfd'
+const backgroundAltHover = darken(0.1, backgroundAlt)
+const error = '#ff8080'
+const errorHover = darken(0.1, error)
+const errorLight = '#ffe6e6'
+const primary = '#0099ff'
+const primaryHover = darken(0.1, primary)
+const primaryLight = '#e6f5ff'
+const success = '#00cc99'
+const successHover = darken(0.1, success)
+const text = '#454545'
+const textAlt = '#fdfdfd'
+const textDark = '#333333'
+const textError = '#ff4d4d'
+const textLight = '#b3b3b3'
+const warning = '#ffcc00'
+const warningHover = darken(0.1, warning)
 
 const colors = {
-    background: '#f5f6fa',
-    backgroundAlt: '#fdfdfd',
-    backgroundAltHover: '#f2f2f2',
-    error: '#ff8080',
-    errorHover: '#ff4d4d',
-    errorLight: '#ffe6e6',
-    primary: '#0099ff',
-    primaryHover: '#007acc',
-    primaryLight: '#e6f5ff',
-    success: '#00cc99',
-    successHover: '#009973',
-    text: '#454545',
-    textAlt: '#fdfdfd',
-    textDark: '#333333',
-    textError: '#ff4d4d',
-    textLight: '#b3b3b3',
-    warning: '#ffcc00',
-    warningHover: '#cca300',
+    background,
+    backgroundAlt,
+    backgroundAltHover,
+    error,
+    errorHover,
+    errorLight,
+    primary,
+    primaryHover,
+    primaryLight,
+    success,
+    successHover,
+    text,
+    textAlt,
+    textDark,
+    textError,
+    textLight,
+    warning,
+    warningHover,
 }
 
 export { colors }
